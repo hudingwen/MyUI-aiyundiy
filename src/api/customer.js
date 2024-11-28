698103b8af10a0387e3fe84714701eae6101a35d
hudingwen <765472804@qq.com>
2024-11-28 19:20:57 +0800

    fix

--- src/api/customer.js
+++ src/api/customer.js
@@ -4,3 +4,12 @@ import request from '@/utils/request.js'
 export const GetNsCustomerInfo = params => {
     return request.get('/api/Nightscout/GetNsCustomerInfo', { params: params });
 };
+// 获取验证码
+export const getCode = params => {
+    return request.get('/api/Login/getCode', { params: params });
+};
+// 启动NS
+export const StartNS = params => {
+    return request.get('/api/Nightscout/StartNS', { params: params });
+};
+
--- src/views/layout/LayoutContainer.vue
+++ src/views/layout/LayoutContainer.vue
@@ -1,26 +1,70 @@
 <script setup>
-import { ElMessageBox } from 'element-plus'
+
 import {
   GetNsCustomerInfo
+  , getCode
+  , StartNS
 } from '@/api/customer.js'
 import { useUserStore, useSettingStore } from '@/stores'
 import { onMounted, ref } from 'vue'
-import router from '@/router'
 
-const userStore = useUserStore()
-const settingStore = useSettingStore()
-
+// 消息框
+import { ElMessage } from 'element-plus'
+import { ElLoading } from 'element-plus'
 
 const curYear = ref('2024')
-const msg = ref('')
+const showHtml = ref('')
+const isExpire = ref(true)
 onMounted(() => {
 
   curYear.value = new Date().getFullYear();
 
-  GetNsCustomerInfo({ host: '' }).then(res => {
-    msg.value = res.data.response
-  })
+  GetUserInfo()
+  refreshCode()
 })
+
+const GetUserInfo = () => {
+
+  GetNsCustomerInfo({ host: '' }).then(res => {
+    showHtml.value = res.data.response.showHtml
+    isExpire.value = res.data.response.isExpire
+  })
+}
+
+const formData = ref({})
+const imgStr = ref('')
+const refreshCode = () => {
+  getCode().then(res => {
+    formData.value.code = ''
+    formData.value.key = res.data.response.key
+    imgStr.value = 'data:image/png;base64,' + res.data.response.code
+  })
+}
+const loadingNs = ref(false)
+const onSubmit = () => {
+  formData.value.host = ''
+  StartNS(formData.value).then(res => {
+    GetNsCustomerInfo()
+    ElMessage.success("启动成功");
+
+    const loading = ElLoading.service({
+      lock: true,
+      text: '启动成功,页面刷新中...',
+      background: 'rgba(0, 0, 0, 0.7)',
+    })
+    loadingNs.value = true
+    setTimeout(() => {
+      loading.close()
+      location.reload(true);
+    }, 5000)
+
+
+  })
+    .catch(() => {
+      formData.value.code = ''
+      refreshCode()
+    })
+}
 
 
 </script>
@@ -29,51 +73,75 @@ onMounted(() => {
   <div style="border-radius: 5px;margin-top: 35px;margin-left: 10px;margin-right: 10px;border: 1px solid red;">
 
 
-    <el-row style="padding: 20px;">
-
-      <el-col v-html="msg">
+    <div style="padding: 20px;">
 
 
+      <div v-if="loadingNs">
+        NS启动中,请稍等......
+      </div>
+      <div>
+        <el-form v-if="!loadingNs && !isExpire" :model="formData" label-width="auto">
+          <el-form-item label="输入右边验证码">
+            <el-input v-model="formData.code" clearable style="width: 100%;" />
+            <el-image @click="refreshCode" style="height: 35px;cursor:pointer;" :src="imgStr">
+              <template #error>
+                <div class="image-slot">
+                  验证码获取中...
+                </div>
+              </template>
+            </el-image>
+          </el-form-item>
+          <el-form-item label="输入NS密码">
+            <el-input v-model="formData.pass" clearable style="width: 100%;" type="password" show-password />
+          </el-form-item>
+          <el-form-item label="　">
+            <el-button type="primary" @click="onSubmit">启动NS</el-button>
+          </el-form-item>
+        </el-form>
+      </div>
+      <div v-if="showHtml && isExpire" style="color: red;">
+        NS已过期,请联系下方微信进行续费!
+      </div>
+      <div v-html="showHtml">
+
+      </div>
 
 
-      </el-col>
 
+      <div v-if="false">
+        <div style="color: red;">温馨提示:</div>
 
-
-      <el-row v-if="false"> 
-        <el-col style="color: red;">温馨提示:</el-col>
-
-        <el-col>如果您打开ns页面跳转到本页面,请无需担心</el-col>
-        <el-col>如果您之前绑定过微信公众号回复"启动"既可</el-col>
-        <el-col>如果没有绑定,您可以联系小羊帮您重新启动ns如下图所示</el-col>
-        <el-col>
+        <div>如果您打开ns页面跳转到本页面,请无需担心</div>
+        <div>如果您之前绑定过微信公众号回复"启动"既可</div>
+        <div>如果没有绑定,您可以联系小羊帮您重新启动ns如下图所示</div>
+        <div>
           <img style="width: 100%;"
             src="https://cdn.aiwanyun.cn/2024/20241109/20241109_263193fccf4b455da4d3dd34f568863b.png">
-        </el-col>
-        <el-col>
-          <el-row>
-            <el-col :span="12">
+        </div>
+        <div>
+          <div>
+            <div :span="12">
               <div style="font-weight: 900;">手机硅基延期远程</div>
               <div>硅基延期远程</div>
               <div>雅培延期远程</div>
               <div>Ns远程Pro服务</div>
-            </el-col>
-            <el-col :span="12">
+            </div>
+            <div :span="12">
               <div style="font-weight: 900;">承接血糖服务</div>
               <div>苹果手机loop</div>
               <div>安卓手机aps(免费)</div>
               <div>苹果硅基延期远程</div>
-            </el-col>
-          </el-row>
-        </el-col>
-        <el-col>
+            </div>
+          </div>
+        </div>
+        <div>
           <div style="font-weight: 900;">各类定制,优惠价格</div>
           <div>硅基探头</div>
           <div>雅培探头</div>
           <div>年卡套餐</div>
           <div>硅基定制手表等</div>
-        </el-col>
-        <el-col>
+        </div>
+        <div>
           <div style="font-weight: 900;">扫码关注小羊微信&微信公众号</div>
           <div><img style="width: 100%;"
               src="https://cdn.aiwanyun.cn/2024/20241109/20241109_9668d65602604f82afe4e545a7a7e934.jpg">
@@ -81,16 +149,16 @@ onMounted(() => {
           <div><img style="width: 100%;"
               src="https://cdn.aiwanyun.cn/2024/20241109/20241109_2a348997385c4cc2a6c30d0948316f26.jpg">
           </div>
-        </el-col>
-        <el-col>
+        </div>
+        <div>
           <div style="text-align: center;">
             © 2023-{{ curYear }} 爱云diy <a href="https://beian.miit.gov.cn" target="_blank"
               rel="nofollow">蜀ICP备15023992号-5</a>
           </div>
-        </el-col>
-      </el-row>
+        </div>
+      </div>
 
-    </el-row>
+    </div>
   </div>
 </template>
 <style lang="scss" scoped></style>
